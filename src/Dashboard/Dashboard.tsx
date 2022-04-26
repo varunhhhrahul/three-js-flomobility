708f23e2b4166c05dbe3a2e6e30af06ae14e4b99
Varun Ramnani <varunhhhrahul@gmail.com>
2022-04-27 00:57:11 +0530

    STABLE: Centered Model and added auto rotate

--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -1,11 +1,17 @@
 import React, { createRef, Suspense, useRef, useState } from "react";
 // import { Canvas } from "@react-three/fiber";
-import { OrbitControls, useGLTF } from "@react-three/drei";
+import {
+  Center,
+  OrbitControls,
+  TrackballControls,
+  useGLTF,
+} from "@react-three/drei";
 import { Group } from "three";
 import * as THREE from "three/src/Three";
 // import { GLTF, GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { useLoader, Canvas } from "@react-three/fiber";
 import { GLTFLoader, GLTF } from "three/examples/jsm/loaders/GLTFLoader";
+import { Spin } from "antd";
 
 interface DashboardProps {}
 
@@ -25,7 +31,7 @@ export const Dashboard: React.FC<DashboardProps> = (props) => {
       <Canvas
         style={{
           height: "100vh",
-          marginTop: "5rem",
+          // marginTop: "5rem",
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
@@ -35,31 +41,40 @@ export const Dashboard: React.FC<DashboardProps> = (props) => {
         frameloop="demand"
         camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 10, 20] }}
       >
-        <Suspense fallback={null}>
-          <ambientLight intensity={0.5} />
-          <spotLight
-            intensity={0.9}
-            angle={1.0}
-            penumbra={0}
-            position={[-25, 1, 25]}
-            castShadow
-          />
-
-          <directionalLight intensity={0.9} position={[-25, 1, 25]} />
-
-          <group
-            ref={group}
-            {...props}
-            dispose={null}
-            scale={[0.12, 0.12, 0.12]}
-          >
-            <primitive
-              object={data.scene}
-              position={[-15, 1, 17]}
-              scale={0.02}
+        <Suspense
+          fallback={
+            <div>
+              <Spin size="large" tip="Loading..." />
+            </div>
+          }
+        >
+          <Center position={[5, 5, 10]}>
+            <ambientLight intensity={0.5} />
+            <spotLight
+              intensity={0.9}
+              angle={1.0}
+              penumbra={0}
+              position={[-25, 1, 25]}
+              castShadow
             />
-          </group>
-          <OrbitControls />
+
+            <directionalLight intensity={0.9} position={[-25, 1, 25]} />
+
+            <group
+              ref={group}
+              {...props}
+              dispose={null}
+              scale={[0.12, 0.12, 0.12]}
+            >
+              <primitive
+                object={data.scene}
+                position={[-15, 1, 17]}
+                scale={0.04}
+              />
+            </group>
+
+            <OrbitControls autoRotate />
+          </Center>
         </Suspense>
       </Canvas>
     </div>
